5d4a30fa2429f4ad6abefc36c578c02070086402
Shunta Saito <shunta.saito@gmail.com>
2025-05-16 20:44:05 +0900

    feat: add Qwen3 (#84)

--- run-openai.py
+++ run-openai.py
@@ -9,24 +9,33 @@ import pfgen
 
 
 def callback(
-    tasks: list[dict[str, str]], params: dict[str, typing.Any], extra_eos_tokens: list[str] | None
+    tasks: list[dict[str, str]],
+    params: dict[str, typing.Any],
+    extra_eos_tokens: list[str] | None,
+    add_no_think: bool,
 ) -> typing.Iterator[str | None]:
     mode = params["mode"]
     temperature = params["temperature"]
     kwargs: dict[str, typing.Any] = {}
     kwargs["base_url"] = os.getenv("OPENAI_BASE_URL")
+    kwargs["api_key"] = os.getenv("OPENAI_API_KEY")
     client = openai.OpenAI(**kwargs)
     for task in tasks:
         kwargs = {}
         if mode == "chat":
+            system_prompt = "/no_think\n" if add_no_think else ""
+            system_prompt += task["system_prompt"]
             kwargs["messages"] = [
-                {"role": "system", "content": task["system_prompt"]},
+                {"role": "system", "content": system_prompt},
                 {"role": "user", "content": task["user_prompt"]},
             ]
         elif mode == "qa":
-            kwargs["messages"] = [{"role": "user", "content": task["prompt"]}]
+            prompt = "/no_think\n" if add_no_think else ""
+            prompt += task["prompt"]
+            kwargs["messages"] = [{"role": "user", "content": prompt}]
         elif mode == "completion":
-            kwargs["prompt"] = task["prompt"]
+            kwargs["prompt"] = "/no_think\n" if add_no_think else ""
+            kwargs["prompt"] += task["prompt"]
         else:
             raise ValueError(f"Unsupported mode: {mode}")
         try:
@@ -51,6 +60,7 @@ def callback(
                     temperature=temperature,
                     top_p=params["top_p"],
                     stop=stop,
+                    stream=False,
                     **kwargs,
                 )
                 yield results.choices[0].text.strip()
@@ -74,13 +84,22 @@ if __name__ == "__main__":
         default="openai/gpt-4o",
         help="OpenAI model name.",
     )
-    parser.add_argument("--temperature", type=float, default=1.0, help="Temperature for sampling.")
+    parser.add_argument("--temperature", type=float, default=0.7, help="Temperature for sampling.")
     parser.add_argument("--num-trials", type=int, default=10, help="Number of trials to run.")
     parser.add_argument("--top-p", type=float, default=0.98, help="Top-p for sampling.")
     parser.add_argument("--extra-eos-tokens", type=str, nargs="+", help="Extra EOS strings")
+    parser.add_argument(
+        "--enable-thinking",
+        action="store_true",
+        help="Enable reasoning when generation",
+    )
     args = parser.parse_args()
 
-    wrapped_callback = partial(callback, extra_eos_tokens=args.extra_eos_tokens)
+    wrapped_callback = partial(
+        callback,
+        extra_eos_tokens=args.extra_eos_tokens,
+        add_no_think=not args.enable_thinking,
+    )
 
     pfgen.run_tasks(
         args.mode,
@@ -90,4 +109,5 @@ if __name__ == "__main__":
         temperature=args.temperature,
         top_p=args.top_p,
         num_trials=args.num_trials,
+        enable_thinking=args.enable_thinking,
     )
